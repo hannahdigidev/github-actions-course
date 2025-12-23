c39c9590865dfa30533b0f8024c3d1df917eb5a7
Hannah.Harvey <118283586+hannahdigidev@users.noreply.github.com>
2025-12-23 14:03:17 +0000

    fix main.py file

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -9,7 +9,7 @@ def ping_url(url, delay, max_trials):
         try:
             response = requests.get(url)
             if response.status_code == 200:
-            print(f"Website {url} is reachable.")
+            print(f"Website {url} is reachable")
             return True
         execept requests.ConnectError:
             print(f"Website {url} is unreachable, Retrying in {delay} seconds....")
