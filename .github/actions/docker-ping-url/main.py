92507d0b27451f1848932586050e80b1f79f0706
Hannah.Harvey <118283586+hannahdigidev@users.noreply.github.com>
2025-12-23 14:17:33 +0000

    fix main.py file

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -2,6 +2,12 @@ import os
 import requests
 import time
 
+
+def set_output(file_path, key, value):
+    with open(file_path, 'a') as file:
+        print(f'{key}={value}', file=file)
+
+
 def ping_url(url, delay, max_trials):
     trials = 0
 
@@ -18,8 +24,9 @@ def ping_url(url, delay, max_trials):
         except requests.exceptions.MissingSchema:
             print(f"Invalid URL format: {url}. Make sure the URL has a valid schema (e.g., http:// or https://)")
             return False
-
+    
     return False
+
 
 def run():
     website_url = os.getenv("INPUT_URL")
@@ -27,12 +34,14 @@ def run():
     max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
 
     website_reachable = ping_url(website_url, delay, max_trials)
-    print("Hello world")
 
-    if not website_reachable: 
-        raise Exeception("Website {website_url} is malformed or unreachable")
+    set_output(os.getenv('GITHUB_OUTPUT'), 'url-reachable', website_reachable)
     
-    print(f"website {website_url} is reachable")
+    if not website_reachable:
+        raise Exception(f"Website {website_url} is malformed or unreachable.")
+
+    print(f"Website {website_url} is reachable.")
+
 
 if __name__ == "__main__":
     run()
