6e8c524d8fb1e79918704b5305788eb8239860b8
Hannah.Harvey <118283586+hannahdigidev@users.noreply.github.com>
2025-12-23 11:23:08 +0000

    Install missing packages

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -2,6 +2,16 @@ const core = require('@actions/core');
 
 
 async function run() {
+    /*
+    1 parse inputs 
+       1.1 - base branch 
+        1.2 . target branch create - pr 
+        1.3 . github token for auth - to create prs
+        1.4 . working dir for checking deps
+    2. execute npm update command in working dir 
+    
+
+     */
     core.info('I am a custom JS action');
 }
 run();
